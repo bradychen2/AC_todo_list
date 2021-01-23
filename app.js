52efae3916cde31bd8ecd161b65c4569d0b2c162
bradychen <bradychen2@gmail.com>
2021-01-23 23:32:05 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send("This is main page")
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`)
+})
