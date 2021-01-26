947326c4aaddad984505cd5c1b9b184110b4c4d0
bradychen <bradychen2@gmail.com>
2021-01-26 19:20:57 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 // Setup connection
 mongoose.connect('mongodb://localhost/todo_list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -20,8 +21,13 @@ db.once('open', () => {
 const app = express()
 const port = 3000
 
+// Establish a layout engine and pass in exphbs parameters
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+// 啟用樣板引擎
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send("This is main page")
+  res.render('index')
 })
 
 app.listen(port, () => {
