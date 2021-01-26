b8b6f35706f136af010ab70a4da2c8b843259c42
bradychen <bradychen2@gmail.com>
2021-01-26 22:57:52 +0800

    feat: add upadte todo function

--- app.js
+++ app.js
@@ -40,11 +40,13 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// Go to create new todo page
 app.get('/todos/new', (req, res) => {
   // 建議都加上 return 
   return res.render('new')
 })
 
+// Check detail
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
   console.log(req.params)
@@ -54,6 +56,7 @@ app.get('/todos/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// Create todo
 app.post('/todos', (req, res) => {
   const name = req.body.name
   // const todo = new Todo({ name })
@@ -63,6 +66,28 @@ app.post('/todos', (req, res) => {
 
   return Todo.create({ name })
     .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// Go to edit page
+app.get('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+
+// Send the edit from
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
