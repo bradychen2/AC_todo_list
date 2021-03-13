0c1c2ea80392ed894365775d17e6f4281daf2b58
bradychen <bradychen2@gmail.com>
2021-03-13 20:55:33 +0800

    feat: add user login page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const todos = require('./modules/todos')
-const user = require('./modules/users')
+const users = require('./modules/users')
 
 // Direct the URL '/' to home module
 router.use('/', home)
 // Direct the URL '/todos' to todos module
 router.use('/todos', todos)
+// Direct the URL '/users/' to users module
+router.use('/users', users)
 
 module.exports = router
