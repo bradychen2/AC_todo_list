49b2460498e5680f02dfed25aef59be94bed59e2
bradychen <bradychen2@gmail.com>
2021-03-14 00:38:08 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 const methodOverride = require('method-override')
+const usePassport = require('./config/passport')
 const routes = require('./routes')
 // Just need to be set, don't need assignment
 require('./config/mongoose')
@@ -15,7 +17,15 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 // Every requests need parsed by bodyParser
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(session({
+  secret: "SuperSecretSecret",
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(methodOverride('_method'))
+usePassport(app)
+
 app.use(routes)
 
 app.listen(PORT, () => {
