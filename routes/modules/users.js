8fda4616ae10bd6d3b97887ad911a52aeb2af97b
bradychen <bradychen2@gmail.com>
2021-03-13 21:26:50 +0800

    feat: add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../../models/user')
+// const User = require('../../models/user')
 
-router.get('/users/login', (req, res) => {
+router.get('/login', (req, res) => {
   res.render('login')
 })
 
